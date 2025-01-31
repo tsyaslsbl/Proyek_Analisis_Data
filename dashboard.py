85f246acf7915cdc475603b6eb2f4802b80f4ff2
Tasya Salsabila <32426273+tsyaslsbl@users.noreply.github.com>
2025-01-31 16:15:31 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -6,50 +6,51 @@ import seaborn as sns
 # Judul Dashboard
 st.title("Dashboard Penyewaan Sepeda")
 
-#Upload dataset
-upload_file = st.file_uploader ("Upload dataset disini", type=["csv"])
+#Load Data
+data_gabungan = pd.read_csv('main_data.csv')
 
-if upload_file:
-    df = pd.read_csv(upload_file)
+# Visualisasi Rata-rata Jumlah Penyewaan Sepeda pada Jam Sibuk
+st.header("Rata-rata Jumlah Penyewaan Sepeda pada Jam Sibuk Setiap Hari")
 
-    #Menampilkan DataFrame
-    st.subheader("Data Penyewaan Sepeda")
-    st.write(df)
+# Identifikasi jam sibuk terlebih dahulu
+# Berdasarkan Explore diatas, kita mengambil TOP 3 peak hours
+peak_hours = data_gabungan.groupby('hr')['cnt_hour'].mean().sort_values(ascending=False).head(3).index
 
-    #Grafik Penyewaan Sepeda per Hari
-    st.subheader("Grafik Penyewaan Sepeda per Hari")
+# Menghitung rata-rata penyewaan harian pada jam sibuk
+peak_hour_rentals = data_gabungan[data_gabungan['hr'].isin(peak_hours)].groupby('weekday_day')['cnt_hour'].mean()
 
-    if 'dteday' in df.columns and 'cnt_day' in df.columns:
-        fig, ax = plt.subplots(figsize=(10, 5))
-        sns.barplot(x=df['dteday'], y=df['cnt_day'], ax=ax)
-        ax.set_ylabel("Total Penyewaan Sepeda")
-        ax.set_xlabel("Tanggal")
-        plt.xticks(rotation=45)
-        st.pyplot(fig)
-    else:
-        st.error("Kolom 'dteday' atau 'cnt_day' tidak ditemukan dalam dataset.")
-    
-    #Grafik Penyewaan Sepeda pada tahun 2011
-    # Buat dataset baru khusus di tahun 2011
-    df["dteday"] = pd.to_datetime(df['dteday'])
-    data_2011 = df[df['dteday'].dt.year == 2011]
+# Agar kelihatan rapi, maka saya mau membulatkan hasil rata-rata penyewaan diatas
+peak_hour_rentals = peak_hour_rentals.round()
 
-    # Dari dataset 2011, kita buatkan grouping data month dan cnt (penyewaan) agar memudahkan menghitung rata-rata
-    data_bulanan_2011 = data_2011.groupby('mnth_day')['cnt_day'].sum().reset_index()
+# Membuat visualisasi
+fig, ax = plt.subplots(figsize=(10, 6))
+peak_hour_rentals.plot(kind='bar', ax=ax)
+ax.set_title("Rata-rata Jumlah Penyewaan Sepeda pada Jam Sibuk Setiap Hari (Dibulatkan)")
+ax.set_xlabel("Hari dalam Seminggu")
+ax.set_ylabel("Rata-rata Jumlah Penyewaan")
+ax.set_xticks(range(7))
+ax.set_xticklabels(['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'])
+ax.tick_params(axis='x', rotation=0)
 
-    #Grafik Penyewaan Sepeda per bulan pada tahun 2011
-    st.subheader("Penyewaan Sepeda per Bulan pada Tahun 2011")
+# Menampilkan plot di Streamlit
+st.pyplot(fig)
 
-    fig, ax = plt.subplots(figsize=(10, 5))
-    sns.barplot(x=data_bulanan_2011['mnth_day'], y=data_bulanan_2011['cnt_day'], palette="Blues", ax=ax)
-    ax.set_xlabel("Bulan")
-    ax.set_ylabel("Total Penyewaan")
-    ax.set_xticks(range(12))  # Pastikan ada 12 bulan
-    ax.set_xticklabels(["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"])
-    st.pyplot(fig)
+# Visualisasi Pengaruh Kondisi Cuaca Terhadap Jumlah Sewa Sepeda pada Hari Kerja dan Akhir Pekan
+st.header("Pengaruh Kondisi Cuaca Terhadap Jumlah Sewa Sepeda pada Hari Kerja dan Akhir Pekan")
 
-else:
-    st.warning ("File CSV kosong")
+# Mengelompokkan data untuk hari kerja dan cuaca
+kerja_cuaca_data = data_gabungan.groupby(['workingday_day','weathersit_day'])['cnt_hour'].mean().unstack()
 
+#Visualisasi
+fig, ax = plt.subplots(figsize=(10, 6))
+kerja_cuaca_data.plot(kind='bar', ax=ax)
+ax.set_title('Pengaruh Kondisi Cuaca Terhadap Jumlah Sewa Sepeda pada Hari Kerja dan Akhir Pekan')
+ax.set_xlabel('Hari Kerja')
+ax.set_ylabel('Rata-rata Jumlah Sewa')
+ax.set_xticks(range(2))
+ax.set_xticklabels(['Akhir Pekan', 'Hari Kerja'])
+ax.legend(title='Kondisi Cuaca')
+ax.tick_params(axis='x', rotation=0)
 
-## PADA GRAFIK PERTAMA, SAYA TIDAK TAHU BAGAIMANA CARA AGAR XLABEL TIDAK SEPANJANG ITU (BISA DIBACA), THATS WHY SAYA MENCOBA BIKIN GRAFIK KEDUA
+# Menampilkan plot di Streamlit
+st.pyplot(fig)
